d21eeae2dd187fa7f66d5008bbca6a196838d788
walidmohamedfarag <walid492003@gmail.com>
2025-10-07 15:25:30 +0300

    add areas

---
+++ ECommerce/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDBContext context = new();
+        public IActionResult Index()
+        {
+            var categories = context.Categroys;
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Categroy categroy)
+        {
+            context.Categroys.Add(categroy);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = context.Categroys.FirstOrDefault(c=>c.Id == id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Categroy categroy)
+        {
+            context.Categroys.Update(categroy);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
