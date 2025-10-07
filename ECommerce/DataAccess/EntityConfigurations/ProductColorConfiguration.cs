78aebd79a6636272e63a36be2bc34882cc6fc6dc
walidmohamedfarag <walid492003@gmail.com>
2025-10-07 15:26:23 +0300

    add data access

---
+++ ECommerce/DataAccess/EntityConfigurations/ProductColorConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.DataAccess.EntityConfigurations
+{
+    public class ProductColorConfiguration : IEntityTypeConfiguration<ProductColor>
+    {
+        public void Configure(EntityTypeBuilder<ProductColor> builder)
+        {
+            builder.HasKey(k => new { k.Color, k.ProductId });
+        }
+    }
+}
