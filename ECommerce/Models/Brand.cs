050c626bebefb6a2d5b80a138cdb62a95db6584b
walidmohamedfarag <walid492003@gmail.com>
2025-10-13 01:13:11 +0300

    modified brand

--- ECommerce/Models/Brand.cs
+++ ECommerce/Models/Brand.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public bool Status { get; set; }
+        public string Image { get; set; } = string.Empty;
     }
 }
