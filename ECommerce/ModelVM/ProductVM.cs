ca76cbc129e06c58104ec98d42b7bb6d804ff075
walidmohamedfarag <walid492003@gmail.com>
2025-10-15 21:10:49 +0300

    add product vm

---
+++ ECommerce/ModelVM/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.ModelVM
+{
+    public class ProductVM
+    {
+        public IEnumerable<Brand> brands { get; set; }
+        public IEnumerable<Categroy> Categroys { get; set; }
+        public IEnumerable<ProductSubImage> ProductSubImages { get; set; }
+        public Product? product { get; set; }
+        public IEnumerable<ProductColor>? productColors { get; set; }
+    }
+}
