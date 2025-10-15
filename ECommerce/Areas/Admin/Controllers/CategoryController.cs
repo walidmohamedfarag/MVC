05d4cc4695db751d921d278d5172e46adb0692bc
walidmohamedfarag <walid492003@gmail.com>
2025-10-15 21:00:01 +0300

    add brand and product and modified category controllers

--- ECommerce/Areas/Admin/Controllers/CategoryController.cs
+++ ECommerce/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Areas.Admin.Controllers
 {
@@ -18,6 +19,7 @@ namespace ECommerce.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult Create(Categroy categroy)
         {
+            //ViewBag.brandid = brandID;
             context.Categroys.Add(categroy);
             context.SaveChanges();
             return RedirectToAction(nameof(Index));
@@ -27,12 +29,23 @@ namespace ECommerce.Areas.Admin.Controllers
         public IActionResult Edit(int id)
         {
             var category = context.Categroys.FirstOrDefault(c=>c.Id == id);
+            if (category is null)
+                return View("NotFoundPage", "Home");
             return View(category);
         }
         [HttpPost]
         public IActionResult Edit(Categroy categroy)
         {
             context.Categroys.Update(categroy);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            var categroy = context.Categroys.FirstOrDefault(c => c.Id == id);
+            if (categroy is null)
+                return View("NotFoundPage", "Home");
+            context.Categroys.Remove(categroy);
             context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
